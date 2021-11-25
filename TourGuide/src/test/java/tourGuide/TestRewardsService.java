caf45d503ace9b22a94a614a8c3ce83d0614d96e
hallier <ahallier@isara.fr>
2021-11-25 18:42:45 +0100

    Test : TestRewardsService update tests of reward service

--- TourGuide/src/test/java/tourGuide/TestRewardsService.java
+++ TourGuide/src/test/java/tourGuide/TestRewardsService.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -23,6 +24,7 @@ import tourGuide.proxies.RewardsProxy;
 import tourGuide.service.RewardsService;
 import tourGuide.service.TourGuideService;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -40,49 +42,59 @@ public class TestRewardsService {
 	@Test
 	public void userGetRewards() {
 		InternalTestHelper.setInternalUserNumber(0);
-		TourGuideService tourGuideService = new TourGuideService(rewardsService);
 
-        LocationResponse locationResponse = new LocationResponse();
-        locationResponse.setLatitude(33.817595D);
-        locationResponse.setLongitude(-117.922008D);
         AttractionResponse attractionResponse = new AttractionResponse(UUID.randomUUID(),"Disneyland", "Anaheim", "CA", 33.817595D, -117.922008D);
         List<AttractionResponse> attractionResponseList = new ArrayList<>();
         attractionResponseList.add(attractionResponse);
         Mockito.when(gpsProxy.getAttractions()).thenReturn(attractionResponseList);
 
         User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+        LocationResponse locationResponse = new LocationResponse();
+        locationResponse.setLatitude(33.817595D);
+        locationResponse.setLongitude(-117.922008D);
         VisitedLocationResponse visitedLocationResponse = new VisitedLocationResponse(user.getUserId(), locationResponse, new Date());
-        Mockito.when(gpsProxy.getUserLocation(user.getUserId())).thenReturn(visitedLocationResponse);
         user.addToVisitedLocationResponseList(visitedLocationResponse);
+
         Mockito.when(rewardsProxy.getRewards(attractionResponse.attractionId, user.getUserId())).thenReturn(100);
-		tourGuideService.trackUserLocation(user);
+
+		rewardsService.calculateRewards(user);
 		List<UserReward> userRewards = user.getUserRewards();
 		assertTrue(userRewards.size() == 1);
 	}
 	
-	/*@Test
+	@Test
 	public void isWithinAttractionProximity() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-		Attraction attraction = gpsUtil.getAttractions().get(0);
-		assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
-	}*/
+        AttractionResponse attractionResponse = new AttractionResponse(UUID.randomUUID(),"Disneyland", "Anaheim", "CA", 33.817595D, -117.922008D);
+
+		LocationResponse locationResponse = new LocationResponse();
+        locationResponse.setLatitude(33.817595D);
+        locationResponse.setLongitude(-117.922008D);
+
+        assertTrue(rewardsService.isWithinAttractionProximity(attractionResponse, locationResponse));
+	}
 	
-	/*@Ignore // Needs fixed - can throw ConcurrentModificationException
+	//@Ignore // Needs fixed - can throw ConcurrentModificationException
 	@Test
 	public void nearAllAttractions() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-		rewardsService.setProximityBuffer(Integer.MAX_VALUE);
+		AttractionResponse attractionResponse = new AttractionResponse(UUID.randomUUID(),"Disneyland", "Anaheim", "CA", 33.817595D, -117.922008D);
+		List<AttractionResponse> attractionResponseList = new ArrayList<>();
+		attractionResponseList.add(attractionResponse);
+		Mockito.when(gpsProxy.getAttractions()).thenReturn(attractionResponseList);
 
 		InternalTestHelper.setInternalUserNumber(1);
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-		
-		rewardsService.calculateRewards(tourGuideService.getAllUsers().get(0));
-		List<UserReward> userRewards = tourGuideService.getUserRewards(tourGuideService.getAllUsers().get(0));
-		//tourGuideService.tracker.stopTracking();
+		TourGuideService tourGuideService = new TourGuideService(rewardsService);
 
-		assertEquals(gpsUtil.getAttractions().size(), userRewards.size());
-	}*/
+		LocationResponse locationResponse = new LocationResponse();
+		locationResponse.setLatitude(33.817595D);
+		locationResponse.setLongitude(-117.922008D);
+		VisitedLocationResponse visitedLocationResponse = new VisitedLocationResponse(tourGuideService.getAllUsers().get(0).getUserId(), locationResponse, new Date());
+		tourGuideService.getAllUsers().get(0).addToVisitedLocationResponseList(visitedLocationResponse);
+
+		Mockito.when(rewardsProxy.getRewards(attractionResponse.attractionId, tourGuideService.getAllUsers().get(0).getUserId())).thenReturn(100);
+
+		rewardsService.calculateRewards(tourGuideService.getAllUsers().get(0));
+		List<UserReward> userRewards = tourGuideService.getAllUsers().get(0).getUserRewards();
+		assertEquals(gpsProxy.getAttractions().size(), userRewards.size());
+	}
 	
 }
